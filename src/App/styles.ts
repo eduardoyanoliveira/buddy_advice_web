4d765204b7bc4b7f5bea8ab8a869f32944af1582
eduardoyanoliveira <63554709+eduardoyanoliveira@users.noreply.github.com>
2022-09-30 19:37:53 -0300

    Centered pages on the app

--- src/App/styles.ts
+++ src/App/styles.ts
@@ -7,7 +7,10 @@ interface IAppTemplate {
 export const AppTemplate = styled.main<IAppTemplate>`
 
     width: 100%;
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
     ${({theme, isLogged}) => css`
         background-image: ${theme.colors.background} ;
         height: ${isLogged ? ' calc(100vh - 80px)' : '100vh'};
